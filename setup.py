e1ecf93ad30f98c8ded497a26698ee667f25b87f
faassen <faassen@startifact.com>
2014-02-27 12:37:48 +0100

    Basic skeleton of more.transaction.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='more.transaction',
+      version = '0.1dev',
+      description="transaction integration for Morepath",
+      author="Martijn Faassen",
+      author_email="faassen@startifact.com",
+      license="BSD",
+      namespace_packages=['more'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'morepath',
+        ],
+      extras_require = dict(
+        test=['pytest >= 2.0',
+              'pytest-cov'],
+        ),
+      )
