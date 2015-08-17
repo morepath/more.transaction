4af01644abcd755b971d821d347f953a8fca3801
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-08-17 13:47:48 +0200

    Back to development: 0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
     io.open('CHANGES.txt', encoding='utf-8').read())
 
 setup(name='more.transaction',
-      version='0.5',
+      version='0.6.dev0',
       description="transaction integration for Morepath",
       long_description=long_description,
       author="Martijn Faassen",
