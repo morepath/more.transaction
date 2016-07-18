cd31182699d3e8af98a399b12afc0515ca7e2218
faassen <faassen@startifact.com>
2016-07-18 14:23:15 +0200

    Back to development: 0.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = '\n'.join((
 ))
 
 setup(name='more.transaction',
-      version='0.7',
+      version='0.8.dev0',
       description="transaction integration for Morepath",
       long_description=long_description,
       author="Martijn Faassen",
