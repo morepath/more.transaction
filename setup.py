51e434dfb11aaa35a93b1ca83777b6fc10ce609c
Henri Hulski <henri.hulski@gazeta.pl>
2016-07-28 00:40:59 +0200

    Add classifiers and fix URL

--- setup.py
+++ setup.py
@@ -6,29 +6,43 @@ long_description = '\n'.join((
     io.open('CHANGES.txt', encoding='utf-8').read()
 ))
 
-setup(name='more.transaction',
-      version='0.8.dev0',
-      description="transaction integration for Morepath",
-      long_description=long_description,
-      author="Martijn Faassen",
-      author_email="faassen@startifact.com",
-      keywords='morepath sqlalchemy zodb transaction',
-      license="BSD",
-      url="http://pypi.python.org/pypi/more.transaction",
-      namespace_packages=['more'],
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'morepath >= 0.15',
-          'transaction',
-      ],
-      extras_require=dict(
-          test=[
-              'coverage',
-              'pytest >= 2.6.0',
-              'pytest-cov',
-              'webtest'
-          ],
-      ))
+setup(
+    name='more.transaction',
+    version='0.8.dev0',
+    description="transaction integration for Morepath",
+    long_description=long_description,
+    author="Martijn Faassen",
+    author_email="faassen@startifact.com",
+    keywords='morepath sqlalchemy zodb transaction',
+    license="BSD",
+    url="https://github.com/morepath/more.transaction",
+    namespace_packages=['more'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Development Status :: 5 - Production/Stable'
+    ],
+    install_requires=[
+        'setuptools',
+        'morepath >= 0.15',
+        'transaction',
+    ],
+    extras_require=dict(
+        test=[
+            'coverage',
+            'pytest >= 2.6.0',
+            'pytest-cov',
+            'webtest'
+        ],
+    ))
